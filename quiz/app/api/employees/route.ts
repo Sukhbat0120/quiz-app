69611a6542f0f21b6814b766537405faca00c329
25LP2243 25LP2243 <25LP2243@G303-CC0130.local>
2025-11-28 12:29:54 +0800

    prisma

--- quiz/app/api/employees/route.ts
+++ quiz/app/api/employees/route.ts
@@ -1,10 +1,13 @@
 import { query } from "@/lib/Connectdb";
 import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
 
 export const GET = async () => {
+  const res = await prisma.employees.findMany();
+  return NextResponse.json(res);
   try {
     const res = await query("SELECT * FROM employees");
-    console.log("RESPONCE!", res.rows);
+    console.log("RESPONSE!", res.rows);
     return NextResponse.json(res.rows);
   } catch (error) {
     console.log("ERROR", error);
